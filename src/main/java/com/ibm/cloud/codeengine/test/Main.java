17d266eccafac02cf15e25bedb7622866416c945
Pravallika Dindukurthi <pravallika.dindukurthi1@ibm.com>
2022-12-01 19:52:32 +0530

    dependencies

--- src/main/java/com/ibm/cloud/codeengine/test/Main.java
+++ src/main/java/com/ibm/cloud/codeengine/test/Main.java
@@ -6,8 +6,8 @@ import org.apache.commons.io.IOUtils;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //IOUtils.write("Specifications :", System.out, Charset.forName("UTF-8"));
+        IOUtils.write("Specifications : \n", System.out, Charset.forName("UTF-8"));
         System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("JVM Tyoe: " + System.getProperty("java.vm.name") + System.getProperty("java.vm.version"));
+        System.out.println("JVM Type: " + System.getProperty("java.vm.name") + System.getProperty("java.vm.version"));
     }
 }
